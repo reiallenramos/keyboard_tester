66a22b68e790888e2f6c03bcd53920a73fc62a8b
Rap Ramos <reiallenramos@gmail.com>
2024-10-07 12:01:34 +1000

    Basic keyboard tester for keys A-Z.

---
+++ app.js
@@ -0,0 +1,43 @@
+class Button {
+  constructor(label) {
+    this.isPressed = false;
+    this.label = label;
+  }
+
+  updateState(state) {
+    this.isPressed = state;
+  }
+}
+
+// A - Z
+const KEYS = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
+const BUTTONS = KEYS.map(key => new Button(key));
+
+function toCharacter(numValue) {
+  return String.fromCharCode(numValue);
+}
+
+const vm = Vue.createApp({
+  data() {
+    return {
+      availableKeys: BUTTONS,
+      inputString: '',
+    }
+  },
+  
+  methods: {
+    updateButtonState(e) {
+      let characterPressed = toCharacter(e.which);
+      let buttonPressed = BUTTONS.find((button => button.label === characterPressed ));
+      if (buttonPressed) {
+        buttonPressed.updateState(true);
+        this.updateButtonArray();
+      }
+    },
+
+    updateButtonArray() {
+      const updatedButtons = [...this.availableKeys];
+      this.availableKeys = updatedButtons;
+    }
+  }
+}).mount('#app')
